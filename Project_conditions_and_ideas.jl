eb385056ddba4723612c5b95ddd1bb85a49cbee3
UlrichAsemann <ulrich.asemann@web.de>
2022-05-31 09:44:01 +0200

    Erweiterung des Projektes

--- Project_conditions_and_ideas.jl
+++ Project_conditions_and_ideas.jl
@@ -8,21 +8,26 @@ Fokus:
 Effizienz z.B. Zeit oder wenig Chaos bis neues Gleichgewicht!
 Kein Kollision miteinander!
 
+Code:
+	- Erweiterung des vorhandenen Codes
+	- kein komplett eigenes neues Projekt
+
 
 Bedingungen:
 Your project deliverable is a Julia module that must fulfil the following constraints:
 	- the demonstration program MUST execute correctly within its own folder
 	- simulates the motion of three or more gravitational bodies
 	- executes an aesthetically pleasing chaotic trajectory
-	- demonstrates that the motion conserves energy
-	- demonstrates that the motion is indeed chaotic
-	- displays explanatory text so that anyone can understand it WITHOUT studying the code!
+	- demonstrates that the motion conserves energy (Energieerhaltung, Berechnung kinetische/potentielle Energie der Körper)
+	- demonstrates that the motion is indeed chaotic (passiert mehr oder weniger automatisch/3-Körper-System)
+	- displays explanatory text so that anyone can understand it WITHOUT studying the code! (Dokumentation des Codes für Erklärungen)
 	- the code must be neat, well documented and easily readable and comprehensible.
 
 	- RK-4 verwenden oder höher (MUST HAVE) (eventuell RK-8)
-	- konzeptuelle Gesamtgeschichte mit innovativem Aspekt
-
-	- weitere Test
+	- konzeptuelle Gesamtgeschichte mit innovativem Aspekt:
+		- Bedrohung der Erde durch den Mond
+		- Ersetzung des Mondes durch einen neuen Körper
+		- Mond wird aus dem System gebracht
 
 Informationen zum Projekt:
 
